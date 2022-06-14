5079932928c784e8e50e9068d3ce1671ef02af2a
Dax Harris <matteovh@gmail.com>
2022-06-14 15:34:38 -0400

    add rust basic

---
+++ rusty-media/target/debug/deps/rusty_media-200cc42e0f42586e.d
@@ -0,0 +1,5 @@
+/mnt/data1/Programming/Rust/rusty-media/rusty-media/target/debug/deps/rusty_media-200cc42e0f42586e.rmeta: src/main.rs
+
+/mnt/data1/Programming/Rust/rusty-media/rusty-media/target/debug/deps/rusty_media-200cc42e0f42586e.d: src/main.rs
+
+src/main.rs:
---
+++ rusty-media/target/debug/deps/rusty_media-ec598e6d59b2deb7.d
@@ -0,0 +1,5 @@
+/mnt/data1/Programming/Rust/rusty-media/rusty-media/target/debug/deps/rusty_media-ec598e6d59b2deb7.rmeta: src/main.rs
+
+/mnt/data1/Programming/Rust/rusty-media/rusty-media/target/debug/deps/rusty_media-ec598e6d59b2deb7.d: src/main.rs
+
+src/main.rs:
